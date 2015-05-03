969da7c8c63dd05303d1100f4f38eb5c55df7d07
forest <forest@tom.com>
2015-05-03 17:26:35 +0800

    sort vector

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -8,4 +9,13 @@ int main()
   v.push_back(1);
   cout << "size of v: " << v.size() << endl;
   cout << "size of v2: " << v2.size() << endl;
+  vector<int> v3;
+  v3.push_back(100);
+  v3.push_back(3);
+  v3.push_back(200);
+  // 默认从小到到大排列
+  sort(v3.begin(), v3.end());
+  cout << v3[0] << endl;
+//  v3.sort();
+//  cout <<v3;
 }
